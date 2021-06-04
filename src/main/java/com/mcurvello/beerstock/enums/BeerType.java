de69f162c82fc8c36633ad7dbdfe816bad57ce1a
Marcio Curvello <marciocurvello@gmail.com>
2021-06-04 12:08:34 -0300

    Beer Class and BeerType Enum

---
+++ src/main/java/com/mcurvello/beerstock/enums/BeerType.java
@@ -0,0 +1,20 @@
+package com.mcurvello.beerstock.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+	LAGER("Lager"),
+	MALZBIER("Malzbier"),
+	WITBIER("Witbier"),
+	WEISS("Weiss"),
+	ALE("Ale"),
+	IPA("Ipa"),
+	STOUT("Stout");
+	
+	private final String description;
+}
